349c9eacf85cf1b63c05fcee73de65d509e5501c
Obada Moustafa Sharbatly <obadamoustafa@gmail.com>
2024-10-25 15:08:33 +0200

    stopped working on the project

--- src/App.jsx
+++ src/App.jsx
@@ -3,21 +3,28 @@ import Home from './pages/home/Home';
 import About from './pages/about/About';
 import Contacts from './pages/contacts/Contacts';
 import Navbar from './components/Navbar';
-import styled from 'styled-components';
 import Footer from './components/Footer';
 import WindowContextProvider from './contexts/WindowContext';
+import HoldingWork from './components/HoldingWork';
 
 function App() {
+  const isClosed = true;
   return (
     <>
       <WindowContextProvider>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/contacts" element={<Contacts />} />
-        </Routes>
-        <Footer />
+        {isClosed ? (
+          <HoldingWork />
+        ) : (
+          <>
+            <Navbar />
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/about" element={<About />} />
+              <Route path="/contacts" element={<Contacts />} />
+            </Routes>
+            <Footer />
+          </>
+        )}
       </WindowContextProvider>
     </>
   );
---
+++ src/components/HoldingWork.jsx
@@ -0,0 +1,24 @@
+import styled from 'styled-components';
+
+const DIV = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background-color: black;
+  color: white;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 30px;
+  text-align: center;
+`;
+function HoldingWork() {
+  //write code here
+
+  return (
+    <DIV>
+      <h1>I stopped working on this project until further notice</h1>
+    </DIV>
+  );
+}
+
+export default HoldingWork;
