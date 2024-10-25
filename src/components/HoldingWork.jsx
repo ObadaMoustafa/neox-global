3f370e0b6c170e503c6efe1a1ee1538e8316bcd3
Obada Moustafa Sharbatly <obadamoustafa@gmail.com>
2024-10-25 15:30:30 +0200

    stop work animated

--- src/components/HoldingWork.jsx
+++ src/components/HoldingWork.jsx
@@ -1,4 +1,8 @@
 import styled from 'styled-components';
+import Image from './Image';
+import gear from '../images/grey-gear.png';
+import { motion } from 'framer-motion';
+import AnimatedText from './AnimatedText';
 
 const DIV = styled.div`
   width: 100vw;
@@ -6,17 +10,39 @@ const DIV = styled.div`
   background-color: black;
   color: white;
   display: flex;
+  row-gap: 40px;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
   padding: 30px;
   text-align: center;
+  font-size: 15pt;
 `;
+
+const GearPhoto = styled(motion.create(Image))`
+  width: 50px;
+`;
+
+const gearVariants = {
+  init: { opacity: 0, rotate: 0 },
+  show: { opacity: 1, rotate: 360 },
+};
+
 function HoldingWork() {
   //write code here
 
   return (
     <DIV>
-      <h1>I stopped working on this project until further notice</h1>
+      <GearPhoto
+        src={gear}
+        variants={gearVariants}
+        animate="show"
+        transition={{
+          duration: 2,
+          repeat: Infinity,
+        }}
+      />
+      <AnimatedText text="I stopped working on this project until further notice" />
     </DIV>
   );
 }
